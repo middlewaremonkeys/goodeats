ac491d417fc37846d22c2cffc4d05b1d4268dc9c
Sam Phillips <armyf35@gmail.com>
2016-12-01 18:57:56 -0500

    Changed to search and options object along with filtered response

--- server/yelpController.js
+++ server/yelpController.js
@@ -10,18 +10,46 @@ yelp.accessToken(clientId, clientSecret).then(response => {
   console.log(e);
 });
 
-var getBuisness = function(query) {
-  return client.search({
-    term: query,
-    location: 'san francisco, ca'
-  }).then(response => {
-    return response.jsonBody;
-  }).catch(e => {
-    console.log(e);
-  });
+var search = function(options) {
+  /*
+  var query = {
+    term: STRING, //optional
+    location: STRING, //required (loc or lat + long)
+    latitude: INT, //required (loc or lat + long)
+    longitude: INT, //required (loc or lat + long)
+    radius: INT, //optional (meters)
+    categories: STRING, //optional (comma delimited)
+    limit: INT, //optional (default 20, max 50)
+    offset: INT, //optional (offset from start)
+    sort_by: STRING, //optional (default best_match, rating, review_count, distance)
+    price: STRING, //optional (1-4) | i.e. '1' | '2,3'
+    open_now: BOOLEAN //optional
+  };
+  */
+
+  return client.search(options)
+    .then(response => {
+      let data = response.jsonBody.businesses;
+
+      return data.map((business) => {
+        return {
+          rating: business.rating,
+          coordinates: business.coordinates,
+          id: business.id,
+          review_count: business.review_count,
+          name: business.name,
+          is_closed: business.is_closed,
+          image: business.image_url,
+          price: business.price,
+          categories: business.categories
+        };
+      });
+    }).catch(e => {
+      console.log(e);
+    });
 };
 
 
 module.exports = {
-  getBuisness: getBuisness
+  search: search
 };
