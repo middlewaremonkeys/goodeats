3aabc0c7ec84117217f3b9c4069de6ced9d38fe3
Sam Phillips <armyf35@gmail.com>
2016-12-01 16:22:18 -0500

    Added /api/yelp/:query route and required yelpController

--- server/server.js
+++ server/server.js
@@ -4,11 +4,23 @@ var bodyParser = require('body-parser');
 var router = require('./router.js');
 var app = express();
 var port = process.env.PORT || 3000;
+var yelpController = require('./yelpController');
 
 app.use(express.static(__dirname + '/../client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/api/yelp/:query', (req, res) => {
+  let query = req.params.query;
+
+  yelpController.getBuisness(query)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 app.get('/', function(req, res) {
   res.send('GOODEATS!!!!');
