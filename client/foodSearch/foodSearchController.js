12d6d0cf2b257278a5d3f973e58d7a45c047a62d
JOHN PACKEL <john.packel@gmail.com>
2016-12-01 19:49:51 -0500

    Food search input box is sending POST request via server & we see the object in console. But need to get it to /api/yelp.

---
+++ client/foodSearch/foodSearchController.js
@@ -0,0 +1,20 @@
+angular.module('goodEats.foodSearch', [])
+
+.controller('foodSearchController', function($scope, $http){
+  // var dataSend = {foodRequest: foodRequest}
+  $scope.data = {}
+  $scope.sendFoodSearch = function(){
+    $http({
+    	method: "POST",
+    	url: '/api/yelp', 
+    	data: JSON.stringify($scope.data)
+    })
+    .then(function(response){
+      console.log(response.data);
+
+    })
+    .catch(function(error) {
+    	console.log(error);
+    })
+  };
+});
