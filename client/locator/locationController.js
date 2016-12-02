6b444b817a98669608b0e06f7cfbe5f61b6b79f8
Mu <zkfire2345@gmail.com>
2016-12-02 16:17:04 -0500

    working on rendering list of restaurants in view

--- client/locator/locationController.js
+++ client/locator/locationController.js
@@ -1,22 +1,87 @@
 //a user should be able to enter their search location
 //by typing in an input box
 
-angular.module('goodEats.locator', [])
+angular.module('goodEats.inputController', [])
 
-.controller('locationController', function($scope, $http) {
+.controller('inputController', function($scope, FoodSearch) {
+	$scope.data = {};
+	$scope.query;
+	$scope.search = function() {
+		FoodSearch.search($scope.query)
+			.then(function(data) {
+				console.log(data);
+				$scope.data = data;
+			})
+	}
 
-	$scope.locator = function(location) {
-		$http({
+})
+.factory('FoodSearch', function($http) {
+	var search = function(options) {
+		return $http({
 			method: 'POST',
-		  url: '/location',
-		  data: { loctation: location }
-		  })
-		.then(function(data) {
-			// var prsed = JSON.parse(data.data);
-			console.log(data);
+		  url: '/api/yelp',
+		  data: options
 		})
-		.catch(function(error) {
-			console.log(error);
+		.then(function(response) {
+			return response.data;
 		})
-	};
+		.catch(function(err) {
+			console.log(err);
+		})
+	}
+
+	return {
+		search: search
+	}
 });
+
+
+	// $scope.foodInArea = function() {
+	// 	FoodSearch.locating({location: location})
+	// 	.then(function(resp) {
+	// 		$scope.data.location = response.data.location
+	// 	})
+	// 	FoodSearch.food({business: business})
+	// 	.then(function(resp){
+	// 		$scope.data.business = response.data.name
+	// 	})
+	// };
+
+	// $scope.foodSearcher = function(food){
+ //    $http({
+ //      method: "POST",
+ //      url: '/api/yelp', 
+ //      data: {term: term}
+ //    })
+ //    .then(function(response){
+ //      console.log("i'm from the foodsearch:: ", response.data);
+ //      $scope.data = response.data;
+
+ //    })
+ //    .catch(function(error) {
+ //      console.log(error);
+ //    })
+ //  };
+
+
+	// var locating = function(location) {
+	// 	$http({
+	// 		method: 'POST',
+	// 	  url: '/api/yelp',
+	// 	  data: { location: location }
+	// 	}).then(function(response) {
+	// 		return response.data;
+	// 	})
+	// }
+
+	// var food = function(food) {
+	// 	$http({
+ //      method: "POST",
+ //      url: '/api/yelp', 
+ //      data: {term: term}
+ //    })
+ //    .then(function(response){
+ //      console.log("i'm from the foodsearch:: ", response.data);
+ //      $scope.data = response.data;
+ //    })
+	// }
--- server/server.js
+++ server/server.js
@@ -27,7 +27,12 @@ app.post('/location', function(req, res) {
 
 app.get('/', function(req, res) {
   res.send('GOODEATS!!!!');
+  console.log('I am successfully receiving a GET request...');
+});
 
+app.post('/api/yelp', function(req, res) {
+	console.log("I AM WORKING");
+  res.send(req.body);
 });
 
 app.listen(port);
