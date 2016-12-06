43ae0ae7e1dbbf95783d874a4d8d5dc02615369d
Mu <zkfire2345@gmail.com>
2016-12-06 11:43:55 -0500

    got the dropdowns to filter the restaurants by price and ratings, changed ratings to stringified forms on filter controller

--- client/filters/filterController.js
+++ client/filters/filterController.js
@@ -3,17 +3,13 @@ angular.module('goodEats.filters', [])
 .controller('filterController', function($scope, Search) {
   $scope.data = {};
   $scope.prices = Search.options.price;
+  
   $scope.priceFilter = Search.priceFilter;
   $scope.displayPrices = ['cheap', 'medium', 'expensive', 'high-end']
 
   $scope.ratings = Search.options.rating;
   $scope.ratingFilter = Search.ratingFilter;
-  $scope.displayRatings = [1, 2, 3, 4, 5];
-
-  $scope.filterExpression = function(thana) {
-    return (thana.dId === $scope.selectedDist.id );
-  };
-
+  $scope.displayRatings = ['you may die', 'eat at your own risk', 'edible', 'delicious', 'experience foodgasms'];
 
 })
 
