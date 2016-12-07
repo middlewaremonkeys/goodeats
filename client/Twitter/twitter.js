a3785d7674d9469a2af1ae6c08d2aac1e44f0556
Svetlin Mladenov <svem@gmx.com>
2016-12-06 20:32:11 -0500

    Fixing the Twitter widget

--- client/Twitter/twitter.js
+++ client/Twitter/twitter.js
@@ -1,13 +1,9 @@
-angular.module('Twitter', [])
+angular.module("Twitter", ["ngSanitize"])
 
 
-.controller('TwitterController', function($scope) {
+.controller("TwitterController", function($scope) {
 
-$scope.test = {
-	"pizza": <a class="twitter-timeline"  href="https://twitter.com/hashtag/pizza" data-widget-id="806294283846295552">#pizza Tweets</a>
-            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>    
-}
+$scope.myHTML = '<a class="twitter-timeline"  href="https://twitter.com/hashtag/pizza" data-widget-id="806294283846295552">#pizza Tweets</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
 
-console.log($scope.test)
 
 })
