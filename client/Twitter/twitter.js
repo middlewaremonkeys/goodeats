7bb25c1ad89fff96a31894c88d6ab509cd0f7b6a
Svetlin Mladenov <svem@gmx.com>
2016-12-07 13:33:57 -0500

    Fixed default widget

--- client/Twitter/twitter.js
+++ client/Twitter/twitter.js
@@ -1,25 +1,45 @@
 angular.module('twitter', [])
 
 .controller('TwitterController', function($scope, $timeout) {
-  $scope.pizza = false;
-  $scope.burger = false;
-  $scope.counter = 0;
+  // $scope.pizza = false;
+  // $scope.burger = false;
+  // $scope.food = false;
+  // $scope.counter = 0;
+  $scope.term = 'burger'
+  $scope.things = {
+  	burger: false,
+  	default: true,
+  	pizza: false
+  }
 
   $scope.bool = function() {
-  	$scope.counter++;
-  	console.log("Counter", $scope.counter)
+  	console.log('click');
+  	// console.log($scope.things[$scope.term])
+  	for (var wid in $scope.things) {
+		$scope.things[wid] = false;
+	}
+  	if ($scope.things.hasOwnProperty($scope.term)) {
+  		$scope.things[$scope.term] = true;
+  	} else {
+  		$scope.things.default = true;
+  	}
+  	// $scope.counter++;
+  	// console.log("Counter", $scope.counter)
 
-  	$timeout(function() {
-   		if ($scope.counter % 3 === 0) {
-  			$scope.pizza = false;
-  			$scope.burger = false;
-  		} else if (!$scope.pizza) {
-  			$scope.pizza = !$scope.pizza;
-  			$scope.burger = false;
-  		} else if (!$scope.burger) {
-  			$scope.burger = !$scope.burger;
-  			$scope.pizza = false;
-  		}
+  	// $timeout(function() {
+   // 		if ($scope.counter % 3 === 0) {
+   // 			$scope.food = true;
+  	// 		$scope.pizza = false;
+  	// 		$scope.burger = false;
+  	// 	} else if (!$scope.pizza) {
+  	// 		$scope.pizza = !$scope.pizza;
+  	// 		$scope.burger = false;
+  	// 		$scope.food = false;
+  	// 	} else if (!$scope.burger) {
+  	// 		$scope.burger = !$scope.burger;
+  	// 		$scope.pizza = false;
+  	// 		$scope.food = false;
+  	// 	}
   		// if ($scope.counter % 3 === 0) {
   		// 	$scope.pizza = false;
   		// 	$scope.burger = false;
@@ -27,7 +47,7 @@ angular.module('twitter', [])
 		  // 	$scope.pizza = !$scope.pizza;
 		  // 	$scope.burger = !$scope.burger;
   		// }
-  	}, 1000);
+  	// }, 1000);
    //  if(!$scope.pizza) {	
    //  return 	$timeout(function(){ $scope.pizza = !$scope.pizza}, 1000);
    // }
@@ -44,5 +64,7 @@ angular.module('twitter', [])
 
 
 
-
+           
+           
+          
 
