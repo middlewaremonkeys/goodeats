868fe1d83a1dc7345960cd882615e422555fe044
JOHN PACKEL <john.packel@gmail.com>
2016-12-07 14:03:47 -0500

    radius in radiusFilter.js, deleted l 15-36 - old filter

--- client/filters/radiusFilter.js
+++ client/filters/radiusFilter.js
@@ -12,27 +12,6 @@ angular.module('goodEats.radiusFilter', [])
          Search.search();
          };
 
-  //radius_filter: between 1-40000
 
-  $scope.radiusFilter = function() {
-
-    return $http({
-      method: 'POST',
-      url: '/location',
-      data:
-      {
-        'term': $scope.food, 
-        'location': $scope.location,
-        'radius_filter': $scope.radius //radius is from the input from in the index.html
-      }
-    })
-    .then(function(data) {
-      console.log('THE DATA', data);
-      return data;
-    })
-    .catch(function(error) {
-      console.log('ERROR:', error);
-    });
-  };
 
 });
