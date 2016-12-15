39c249ecc9f2994c146bad6f630ee34500f97475
Svetlin Mladenov <svem@gmx.com>
2016-12-14 22:52:02 -0500

    polished server.js

--- server/router.js
+++
@@ -1,7 +0,0 @@
-//WE DIDN'T USE A ROUTER BUT MAYBE THE NEXT GROUP CAN USE IT FOR THEIR LEGACY
-
-var router = require('express').Router();
-
-//var differentModules = require('/from somewhere');
-
-module.exports = router;
